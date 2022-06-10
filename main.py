a4526d45c8d56b795a9fea611734e1c1390d657b
KasymkhanKhubiyev <kasymkhankhubievnis@gmail.com>
2022-06-10 23:27:06 +0700

    Commit Changes

--- main.py
+++ main.py
@@ -20,20 +20,61 @@ def Roman_to_integer(string):
                 result -= numbers[i]
     return result
 
+def Integer_to_Roman(num):
+    num = num
+    string = str(num)
+    numbers = []
+    result = ''
+    Num_lib = {1: 'I', 5: 'V', 10: 'X', 50: 'L', 100: 'C', 500: 'D', 1000: 'M'}
+
+    for i in range(len(string) - 1, -1, -1):
+        diff = num // 10 ** i
+        num = num - diff * 10 ** i
+        latter = Num_lib[10 ** i]
+        if diff == 5:
+            if latter == 'I':
+                result += 'V'
+            elif latter == 'X':
+                result += 'L'
+            elif latter == 'C':
+                result += 'D'
+        elif diff <= 3:
+            result += latter * diff
+        elif diff == 4:
+            if latter == 'I':
+                result += 'IV'
+            elif latter == 'X':
+                result += 'XL'
+            elif latter == 'C':
+                result += 'CD'
+        elif diff == 9:
+            if latter == 'I':
+                result += 'IX'
+            elif latter == 'X':
+                result += 'XC'
+            elif latter == 'C':
+                result += 'CM'
+        elif diff > 5:
+            if latter == 'I':
+                result += 'V' + 'I' * (diff - 5)
+            elif latter == 'X':
+                result += 'L' + 'X' * (diff - 5)
+            elif latter == 'C':
+                result += 'D' + 'C' * (diff - 5)
+
+    return result
 
 if __name__ == '__main__':
 
     string = 'LVIII'
     result = Roman_to_integer(string)
+    print(string, result)
 
-    num = 1954
-    string = str(num)
-    numbers = []
-    Num_lib = {1:'I',5: 'V',10: 'X',50: 'L',100: 'C',500: 'D',1000: 'M'}
+    num = 1994
+    result1 = Integer_to_Roman(num)
+    print(num, result1)
 
-    for i in range(len(string)-1, -1, -1):
-        diff = num // 10**i
-        latter = Num_lib[10**i]
+
 
 
 
